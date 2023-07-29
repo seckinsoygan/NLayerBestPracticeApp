ea2dd2f45a2e106cf5c4e86ce9ecee863374467b
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-07-29 09:30:48 +0300

    Server layer started

--- Core/Interfaces/IService.cs
+++ Core/Interfaces/IService.cs
@@ -7,10 +7,11 @@ namespace Core.Interfaces
 		Task<T> GetByIdAsync(int id);
 		Task<IEnumerable<T>> GetAllAsync();
 		IQueryable<T> Where(Expression<Func<T, bool>> expression);
-		bool AnyAsync(Expression<Func<T, bool>> expression);
-		Task AddRangeAsync(IEnumerable<T> entities);
+		Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+		Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
 		Task<T> AddAsync(T entity);
 		Task UpdateAsync(T entity);
 		Task RemoveAsync(T entity);
+		Task RemoveRangeAsync(IEnumerable<T> entities);
 	}
 }
